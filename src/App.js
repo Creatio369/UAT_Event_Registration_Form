51c99509fdb6b77c9968058e84449c5ba8ba3007
Creatio369 <manisha@1solution.dev>
2024-11-27 11:51:35 +0530

    Made changes for UAT deployment

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ const EventRegistrationForm = () => {
         .split(",")
         .filter((item) => item !== "Other (free text)")
         .concat(formData.otherDietaryRequirement || [])
-        .join("  ,"),
+        .join("  , "),
     };
 
     if (!filteredFormData.OtherCPD) {
